2ff31b86dcf9acbfb5e6ae50f6e456a03480cfbd
IVaN Ivanovi4 <eng.vano@yandex.ru>
2023-02-04 12:25:30 +0300

    Add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#008cf0',
+        'dark-blue': '#006ebd',
+        'light-blue': '#00bfff'
+      },
+      fontFamily: {
+        'roboto': ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
